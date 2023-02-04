1fbf4c6cb316766e1718223ccd16ab5f609397a0
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-02-04 17:49:25 -0300

    testing splip

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace udemy1
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string[] vet  = s.Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+
+
+
+            /*  string frase = Console.ReadLine();
+
+             string x = Console.ReadLine();
+
+             string y = Console.ReadLine();
+
+             string z = Console.ReadLine();
+
+
+             Console.WriteLine("Você digitou:");
+             Console.WriteLine(frase);
+             Console.WriteLine(x);
+             Console.WriteLine(y);
+             Console.WriteLine(z);*/
+        }
+    }
+}
